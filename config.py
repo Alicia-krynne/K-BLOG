fbd37d54dd0581cca53db2b3b4a7c540779002f6
Alicia-krynne <alicemacrineadhiambo@gmail.com>
2021-05-05 20:41:50 +0300

    add photos dest

--- config.py
+++ config.py
@@ -9,7 +9,10 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
     
     # simple mde  configurations
     SIMPLEMDE_JS_IIFE = True
